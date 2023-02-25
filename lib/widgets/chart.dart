370f25f116bed8760bd6ebb04882cdb54aec3017
Eduardo Comploma Jr <eduardocomplomajr@gmail.com>
2023-02-25 17:51:44 +0800

    105. Populating the Chart with Values

--- lib/widgets/chart.dart
+++ lib/widgets/chart.dart
@@ -1,6 +1,7 @@
 import "package:flutter/material.dart";
 import "package:intl/intl.dart";
 
+import "../widgets/chart_bar.dart";
 import "../models/transaction.dart";
 
 class Chart extends StatelessWidget {
@@ -30,6 +31,12 @@ class Chart extends StatelessWidget {
     });
   }
 
+  double get totalSpending {
+    return groupedTransactionValues.fold(0.0, (sum, item) {
+      return sum + (item['amount'] as double);
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
@@ -37,7 +44,13 @@ class Chart extends StatelessWidget {
       margin: EdgeInsets.all(20),
       child: Row(
         children: groupedTransactionValues.map((data) {
-          return Text("${data['day']}: ${data['amount']}");
+          return ChartBar(
+            data['day'] as String?,
+            data['amount'] as double?,
+            totalSpending == 0.0
+                ? 0.0
+                : (data['amount'] as double) / totalSpending,
+          );
         }).toList(),
       ),
     );
